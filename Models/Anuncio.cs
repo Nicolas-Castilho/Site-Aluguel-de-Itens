50138b860fd8f78863b96d91ff206c1b84c8d646
Nicolas Castilho <valdaircastilho@gmail.com>
2024-01-24 23:19:57 -0300

    Upload

---
+++ Models/Anuncio.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aluguel.Models
+{
+    public class Anuncio
+    {
+        [Key]
+        public int id { get; set; }
+        public float vlitem { get; set; }
+        public int diasAluguel { get; set; }
+        public string modelo { get; set; }
+        public string CEP { get; set; }
+        public string Rua { get; set; }
+        public string? Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string? Complemento { get; set; }
+        public string contato { get; set; }
+
+        public string NomeFantasia { get; set; }
+
+        //AspNetUsers
+        public string iduser { get; set; }
+
+        //Item
+        [ForeignKey("itemid")]
+        public int itemid { get; set; }
+
+        public Item? item { get; set; }
+
+        public int categoriaid { get; set; }
+        public Categoria? categoria { get; set; }
+        //Tipo
+        public int tipoid { get; set; }
+        public Tipo? tipo { get; set; }
+
+        public List<Anuncio> GetAnuncios()
+        {
+            List<Anuncio> anuncios = new List<Anuncio>();
+            return anuncios;
+        }
+
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,39 @@
+﻿using aluguel.ViewModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace aluguel.Models
+{
+    public class Item
+    {
+        [Key]
+        public int id { get; set; }
+        public string nmitem { get; set; }
+        public string dsitem { get; set; }
+        public DateTime dtcriacao { get; set; }
+        public string snativo { get; set; }
+        public string imagem1 { get; set; }
+        [AllowNull]
+        public string? imagem2 { get; set; }
+        [AllowNull]
+        public string? imagem3 { get; set; }
+        [AllowNull]
+        public string? imagem4 { get; set; }
+        [AllowNull]
+        public string? imagem5 { get; set; }
+
+        //AspNetUsers
+        public string iduser { get; set; }
+        //Categoria
+        [NotMapped]
+        public ItemViewModel? itemViewModel { get; set; }
+
+        private List<Item> GetItems()
+        {
+            List<Item> items = new List<Item>();
+            return items;
+        }
+    }
+
+}
